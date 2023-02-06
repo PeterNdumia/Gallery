08b63f2c125d2deb0fe6a5e99b0d38252ddaacd9
Peter Ndumia <ndegwandumia@gmail.com>
2023-02-06 17:23:37 +0300

    feat
    Add Google Authentication

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './Pages/LandingPage';
+import AppContext from './AppContext';
 
 function App() {
+    const [user,setUser] = useState({});
+    const [isLoggedIn,setIsLoggedIn]= useState(false);
     return (
         <div className=''>
-            <Routes>
-                <Route path="/"  element={<LandingPage/>}></Route>
+            <AppContext.Provider value={{user, setUser, isLoggedIn, setIsLoggedIn}}>
+                <Routes>
+                    <Route path="/"  element={<LandingPage/>}></Route>
     
-            </Routes>
+                </Routes>
+            </AppContext.Provider>
         </div>
     );
 }
--- src/Pages/LandingPage/index.js
+++ src/Pages/LandingPage/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AboutSection from './components/AboutSection';
 import Footer from './components/Footer';
 import GettingStartedSection from './components/GettingStartedSection';
-import NavBar from './components/NavBar';
+import NavBar from '../../components/NavBar';
 
 function LandingPage() {
     return (
