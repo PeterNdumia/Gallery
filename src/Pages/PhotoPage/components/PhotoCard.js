5b7e6eb94f76b142aef7a749349c2078a908df74
Peter Ndumia <ndegwandumia@gmail.com>
2023-02-07 14:26:31 +0300

    fix
    Fix resposiveness of Photo Page

--- src/Pages/PhotoPage/components/PhotoCard.js
+++ src/Pages/PhotoPage/components/PhotoCard.js
@@ -32,20 +32,20 @@ function PhotoCard() {
     return (
 
         <div className='container flex flex-col justify-center w-full'>
-            <p className='text-2xl md:text-3xl text-center mt-24'>{photoTitle}</p>
+            <p className='text-lg md:text-3xl text-center mt-24'>{photoTitle}</p>
 
-            <div className='container rounded-lg shadow-lg bg-white max-w-4xl items-center justify-center p-1 flex flex-row self-center mt-12'>
-                <img src={chosenPhoto.url} className='max-w-lg' />
-                <div className='items-center flex flex-col ml-6 '>
-                    <form className='max-w-sm' >
-                        <input className='h-[50px] max-w-[200px] rounded-md border-2 border-primary px-2' type="text" value={photoTitle} onChange={(e)=> handle(e)} /> 
+            <div className='container rounded-lg shadow-lg bg-white max-w-sm md:max-w-4xl items-center justify-center p-1 flex flex-row self-center mt-12'>
+                <img src={chosenPhoto.url} className='md:max-w-lg max-w-[200px]' />
+                <div className='items-center flex flex-col md:ml-6 ml-4 '>
+                    <form className='md:max-w-sm ' >
+                        <input className='h-[50px] md:max-w-[200px] max-w-[100px] rounded-md border-2 border-primary px-2' type="text" value={photoTitle} onChange={(e)=> handle(e)} /> 
                     </form>
                     <button onClick={()=>{
                         console.log('edit photo title');
                         console.log(chosenPhoto.id);
                         updateTitle();
                
-                    }} className='px-2 rounded-md border-2 border-primary bg-white text-primary py-1 mt-5 hover:bg-primary hover:text-white'>Update Title</button>
+                    }} className='px-2 rounded-md border-2 border-primary bg-white text-sm  text-primary py-1 mt-5 hover:bg-primary hover:text-white'>Update Title</button>
                 </div>
             </div>
 
